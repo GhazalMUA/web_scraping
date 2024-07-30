2fd8e526d8ba79c9bc128182ae6988bd1578bda8
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-07-30 11:17:32 +0330

    qualities

--- video_downloader.py
+++ video_downloader.py
@@ -2,6 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
+
+
+qualities={
+    '144':0,
+    '240':1,
+    '360':2,
+    '480':3,
+    '720':4,
+    '1080':5
+}
+
+
+
 class Scraper:
     def __init__(self,url,quality=None):
         self.url= url
@@ -15,5 +29,10 @@ class Scraper:
         return links
     
     
-s=Scraper('https://www.namasha.com/v/y5kreMJe')   
-print(s.get_all_links()) 
+    '''
+        tooye website namasha keyfiatha az pain be bala ziad mishan va 
+        age y keyfiati vojod nadashte bashe, az bala vojod nadare.
+        baray inkar miaym dictionary quality misazim va avale codemon mizarimesh
+    '''
+    
+    
