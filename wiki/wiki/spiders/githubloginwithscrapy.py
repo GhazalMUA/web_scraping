c2108aaf084edf524bd4551ce69605f5b35feb5b
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-08-14 11:14:47 +0330

    login to github and read email from bio in my profile witg scrapy

---
+++ wiki/wiki/spiders/githubloginwithscrapy.py
@@ -0,0 +1,67 @@
+import scrapy
+
+'''
+    mikhaym ba scrapy login koni toye github
+    hameye vorodi haye forme github ro handle konim
+    befrestim be forme login esh
+    username o password ke mibinim dare vase login
+    kardan vali y seri input ba type hidden ham dare k maa
+    nmibinim tooye inspect esh hastand.
+    ona ro ham baayd handle konim
+    baazi az in hidden ha ham value nadaran baayad in ghazie ro
+    ham handle konim va vaseye onai k value nadraan `unknown` befrestim.
+    tooye inspect ha ghesmate form ha chizi k ahamiat dare name e on field e input hasatesh hastesh
+'''
+
+class GithubloginwithscrapySpider(scrapy.Spider):
+    name = "githubloginwithscrapy"
+    allowed_domains = ["github.com"]
+    start_urls = ["https://github.com/login"]
+    '''
+        in custom settings i ke in paain neveshtam vase ine ke tooye settings
+        e in porojeye scrapy ke alan baze man pipeline daram ke faal shode va
+        makhsose population hastesh vase file e countrydetail.py ke population e
+        country haro raftim crawl kardim alan chon tooye hamon poroje ye file
+        spider sakhtim, error i ke mide mide fekr mikone on pipeline male in file.
+        so vaseye inke on pipeline ro dar nazar nagirim settings emon ro override kardim.
+    '''
+    custom_settings = {
+        'ITEM_PIPELINES': {}
+    }
+    def parse(self, response , **kwargs):
+        data = {'login':'ghazalmua' , 'password':'8384@Azsd2'}
+        
+        for hidden in response.css('input[type="hidden"]'):
+            data[hidden.attrib['name']]=hidden.attrib.get('value','unknown')
+        
+        #ba in do khate pain mitonestim bebinim tooye data chi hast(key va vlue) esh ro mitonestim bbinim
+        # print(200*'&')    
+        # yield data    
+
+        '''
+            url bayad behesh bedi. mige in formi k alan poresh kardam be che addressi submitsh konam?
+            address mishe hamon safeye   https://github.com   slash session hala chera session? age berimm
+            tooye inspect e safeye login e githunb, tooye bakhshe form action ro gozashgte session. yani mire 
+            dakhele session pas addresse url nahaimon mishe https://github.com/session   
+            
+            ye arguman e dige ham formdata hastesh ke bayad hamin dictionary ke haavie user,pass,field 
+            haye hidden hastesh ro behesh bedim
+            
+            akharin arguman callback hastesh mige hala k in dictionary e data ro be addressi ke dadi ferestadam, bayad badesh chekar konam?
+            migim boro karai k tooye function e after_login() behet goftim ro anjam bede.
+            
+             
+        '''
+        return scrapy.FormRequest(url='https://github.com/session', formdata=data, callback=self.after_login)
+    
+    def after_login(self,response):
+        '''
+            plan ine vaghti login shod bere tooye saafeye profile am va email am ro az tooye bio am bekhoone
+        '''
+        yield response.follow(url='https://github.com/GhazalMUA' ,callback=self.profile )
+        
+    def profile(self,response):
+        email = response.xpath('/html/body/div[1]/div[5]/main/div/div/div[1]/div/div/div[3]/div[2]/ul/li[2]/a/text()').get()
+        print(100*'%')
+        yield {'info':email}    
+        
--- wiki/wiki/spiders/matalan.py
+++ wiki/wiki/spiders/matalan.py
@@ -8,16 +8,10 @@ class MatalanSpider(CrawlSpider):
     allowed_domains = ["www.matalan.co.uk"]
     start_urls = ["https://www.matalan.co.uk/womens/dresses.list"]
 
-    rules = (Rule(LinkExtractor(allow=r"/clothing/"), callback="parse_item", follow=False),)
+    rules = (Rule(LinkExtractor(allow=r"/clothing/"), callback="parse_item", follow=True),)
 
     def parse_item(self, response):
         item = {}
-        # item['product_id'] = response.xpath('//*[@id="productDetails-b0766f1d-0ebe-49ce-920c-3a29fd34e9e5"]/text()').get()        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        # item['product_id'] = response.xpath('//*[@id="productDetails"]/@data-product-id').get()   
-        # item['product_id'] = response.xpath('//*[contains(@id, "productDetails")]/div/section//div[@data-product-id]/@data-product-id').get()
         item['product_id'] = response.xpath('//*[contains(@id, "productDetails")]/div/section/div/div[4]/div/text()').get()
 
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        
-        #item["description"] = response.xpath('//div[@id="description"]').get()
         return item
